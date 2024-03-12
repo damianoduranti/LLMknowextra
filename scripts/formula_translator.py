93e9663efe3f23a3b2085eadaacf7c98eee260ad
Damiano Duranti <42733349+damianoduranti@users.noreply.github.com>
2024-03-12 12:50:09 +0100

    Implemented f2i translation

--- scripts/formula_translator.py
+++ scripts/formula_translator.py
@@ -1,11 +1,11 @@
-from pylogics.parsers import parse_ltl
 from llm_utils import *
-from prompt_generator import *
+from utils import *
 from smv_generator import *
+from pylogics.parsers import parse_ltl
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def parse_response(response):
+def f2i(response):
     """
     Parses the response from the LLM.
     
@@ -13,96 +13,29 @@ def parse_response(response):
     - response (str): Response from the LLM.
     
     Returns:
-    - str: Parsed response.
+    - str: Parsed response to f2i.
     """
-    parsed_response = parse_ltl(response)
-    logging.info(f"Parsed response: {parsed_response}")
+    parsed_response = to_f2i(parse_ltl(response))
     
     return str(parsed_response)
 
-def f2i(formula):
-    """
-    Translates a formula using the end-of-time translation.
-    
-    Parameters:
-    - formula (str): Formula to be translated.
-    
-    Returns:
-    - str: Translated formula.
-    """
-    if formula[0].isalpha():
-        return formula
-    elif formula.startswith("(not "):
-        subformula = formula[5:-1]
-        return f"!{f2i(subformula)}"
-    elif formula.startswith("(always "):
-        subformula = formula[8:-1]
-        return f"G({f2i(subformula)} | End)"
-    elif formula.startswith("(eventually "):
-        subformula = formula[12:-1]
-        return f"F({f2i(subformula)} & !End)"
-    elif formula.startswith("(next "):
-        subformula = formula[6:-1]
-        return f"X({f2i(subformula)} & !End)"
-    elif formula.startswith("(weak_next "):
-        subformula = formula[11:-1]
-        return f"!X!({f2i(subformula)} & !End)"
-    elif formula.startswith("(and "):
-        formula = formula[5:-1]
-        if formula[0] == "(":
-            subformula1 = formula[1:formula.index(") ")]
-            subformula2 = formula[formula.index(") ") + 2:]
-        else:
-            subformula1 = formula[:formula.index(" ")]
-            subformula2 = formula[formula.index(" ") + 1:]
-        return f"({f2i(subformula1)} & {f2i(subformula2)})"
-    elif formula.startswith("(or "):
-        formula = formula[4:-1]
-        if formula[0] == "(":
-            subformula1 = formula[1:formula.index(") ")]
-            subformula2 = formula[formula.index(") ") + 2:]
-        else:
-            subformula1 = formula[:formula.index(" ")]
-            subformula2 = formula[formula.index(" ") + 1:]
-        return f"({f2i(subformula1)} | {f2i(subformula2)})"
-    elif formula.startswith("(implies "):
-        formula = formula[9:-1]
-        if formula[0] == "(":
-            subformula1 = formula[1:formula.index(") ")]
-            subformula2 = formula[formula.index(") ") + 2:]
-        else:
-            subformula1 = formula[:formula.index(" ")]
-            subformula2 = formula[formula.index(" ") + 1:]
-        return f"({f2i(subformula1)} -> {f2i(subformula2)})"
-    elif formula.startswith("(until "):
-        formula = formula[7:-1]
-        if formula[0] == "(":
-            subformula1 = formula[1:formula.index(") ")]
-            subformula2 = formula[formula.index(") ") + 2:]
-        else:
-            subformula1 = formula[:formula.index(" ")]
-            subformula2 = formula[formula.index(" ") + 1:]
-        return f"({f2i(subformula1)} U ({f2i(subformula2)} & !End))"
-
 response = "G (p -> X[!] F q)"
-response1 = "p U (X[!] q)"
+response1 = "(F(p) & !(G(s))) U (q & (r -> !(X[!] (F(t)))))"
 response2 = "G(G(F(p)))"
 response3 = "(X[!](p U q)) U (r U (G(s)))"
 response4 = "G((p & q) -> (p | q))"
 response5 = "(p -> q) U (r U s)"
-parsed_response = parse_response(response)
-parsed_response1 = parse_response(response1)
-parsed_response2 = parse_response(response2)
-parsed_response3 = parse_response(response3)
-parsed_response4 = parse_response(response4)
-parsed_response5 = parse_response(response5)
+parsed_response = f2i(response)
+parsed_response1 = f2i(response1)
+parsed_response2 = f2i(response2)
+parsed_response3 = f2i(response3)
+parsed_response4 = f2i(response4)
+parsed_response5 = f2i(response5)
 
-#logging.info(f"Formula: {parsed_response}")
-print(f2i(parsed_response))
-print(f2i(parsed_response1))
-print(f2i(parsed_response2))
-print(f2i(parsed_response3))
-print(f2i(parsed_response4))
-print(f2i(parsed_response5))
-
+logging.info(f"Formula: {parsed_response}")
+logging.info(f"Formula: {parsed_response1}")
+logging.info(f"Formula: {parsed_response2}")
+logging.info(f"Formula: {parsed_response3}")
+logging.info(f"Formula: {parsed_response4}")
+logging.info(f"Formula: {parsed_response5}")
 
---
+++ scripts/utils.py
@@ -0,0 +1,124 @@
+import functools
+from typing import Sequence
+
+from pylogics.exceptions import PylogicsError
+from pylogics.syntax.base import (
+    AbstractAtomic,
+    And,
+    Equivalence,
+    FalseFormula,
+    Formula,
+    Implies,
+    Logic,
+    Not,
+    Or,
+    TrueFormula,
+)
+from pylogics.syntax.ltl import Always, Eventually, Next
+from pylogics.syntax.ltl import PropositionalFalse as LTLPropositionalFalse
+from pylogics.syntax.ltl import PropositionalTrue as LTLPropositionalTrue
+from pylogics.syntax.ltl import Release, StrongRelease, Until, WeakNext, WeakUntil
+
+@functools.singledispatch
+def to_f2i(formula: Formula) -> str:
+    """Transform a formula to a parsable string."""
+    raise PylogicsError(f"formula '{formula}' is not supported")
+
+def _map_operands_to_f2i(operands: Sequence[Formula]):
+    """Map a list of operands to a list of strings (with brackets)."""
+    return map(lambda sub_formula: f"({to_f2i(sub_formula)})", operands)
+
+@to_f2i.register(And)
+def to_f2i_and(formula: And) -> str:
+    """Transform an And into string."""
+    return " & ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(Or)
+def to_f2i_or(formula: Or) -> str:
+    """Transform an Or into string."""
+    return " | ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(Not)
+def to_f2i_not(formula: Not) -> str:
+    """Transform a Not into string."""
+    return f"~({to_f2i(formula.argument)})"
+
+@to_f2i.register(Implies)
+def to_f2i_implies(formula: Implies) -> str:
+    """Transform an Implies into string."""
+    return " -> ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(Equivalence)
+def to_f2i_equivalence(formula: Equivalence) -> str:
+    """Transform an Equivalence into string."""
+    return " <-> ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(AbstractAtomic)
+def to_f2i_atomic(formula: AbstractAtomic) -> str:
+    """Transform an atomic formula into string."""
+    return formula.name
+
+@to_f2i.register(TrueFormula)
+def to_f2i_logical_true(formula: TrueFormula) -> str:
+    """Transform the "tt" formula into string."""
+    return "tt" if formula.logic != Logic.PL else "true"
+
+@to_f2i.register(FalseFormula)
+def to_f2i_logical_false(formula: FalseFormula) -> str:
+    """Transform the "ff" formula into string."""
+    return "ff" if formula.logic != Logic.PL else "false"
+
+@to_f2i.register
+def to_f2i_ltl_propositional_true(_formula: LTLPropositionalTrue) -> str:
+    """Transform the "tt" formula into string."""
+    return "true"
+
+@to_f2i.register
+def to_f2i_ltl_propositional_false(_formula: LTLPropositionalFalse) -> str:
+    """Transform the "ff" formula into string."""
+    return "false"
+
+@to_f2i.register(Next)
+def to_f2i_next(formula: Next) -> str:
+    """Transform a next formula into string."""
+    return f"X[!]({to_f2i(formula.argument)} & !End)"
+
+@to_f2i.register(WeakNext)
+def to_f2i_weak_next(formula: WeakNext) -> str:
+    """Transform a weak next formula into string."""
+    return f"X[!]({to_f2i(formula.argument)} | End)"
+
+@to_f2i.register(Until)
+def to_f2i_until(formula: Until) -> str:
+    """Transform an Until formula into string with & !End appended to the second operand."""
+    operands_str = list(_map_operands_to_f2i(formula.operands))
+    operands_str[1] = f"({operands_str[1]} & !End)"
+    return " U ".join(operands_str)
+
+# @to_f2i.register(WeakUntil)
+# def to_f2i_weak_until(formula: WeakUntil) -> str:
+#     """Transform a weak until formula into string."""
+#     return " W ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(Release)
+def to_f2i_release(formula: Release) -> str:
+    """Transform a release formula into string with & !End appended to the first operand and | End appended to the second"""
+    operands_str = list(_map_operands_to_f2i(formula.operands))
+    operands_str[0] = f"({operands_str[0]} & !End)"
+    operands_str[1] = f"({operands_str[1]} | End)"
+    return " R ".join(operands_str)
+
+# @to_f2i.register(StrongRelease)
+# def to_f2i_strong_release(formula: StrongRelease) -> str:
+#     """Transform a strong release formula into string."""
+#     return " M ".join(_map_operands_to_f2i(formula.operands))
+
+@to_f2i.register(Eventually)
+def to_f2i_eventually(formula: Eventually) -> str:
+    """Transform a eventually formula into string with & !End appended."""
+    return f"F({to_f2i(formula.argument)} & !End)"
+
+@to_f2i.register(Always)
+def to_f2i_always(formula: Always) -> str:
+    """Transform a always formula into string with | End appended."""
+    return f"G({to_f2i(formula.argument)} | End)"
