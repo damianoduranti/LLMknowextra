c778bbc35c06cc3b044e40d6d90f9afa17b7eb0e
Damiano Duranti <42733349+damianoduranti@users.noreply.github.com>
2024-04-30 16:25:05 +0200

    Added test cases

--- scripts/dl_concept.py
+++ scripts/dl_concept.py
@@ -3,6 +3,7 @@ import os
 import json
 import time
 import csv
+import re
 from prompt_generator import generate_dl_prompts_from_json
 from llm_utils import send_prompt, load_api_keys, set_openai_api_configurations
 from DL_concept.concept_verifier import evaluator, concept_verifier, load_ontology
@@ -12,9 +13,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def create_output_dir(trace_path, folder_name):
     parts = trace_path.split('/')
     constraint_type = parts[2]
-    file_id = os.path.splitext(parts[3])[0] 
-
-    output_dir = os.path.join("output", "DL_concept", constraint_type, file_id, folder_name)
+    
+    output_dir = os.path.join("output", "DL_concept", constraint_type, folder_name)
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -33,56 +33,56 @@ def dl_concept(instances_path, ontology_path, max_attempts=5):
         response(str): A correct DL concept if found, None otherwise.
         int: The number of attempts made until a correct concept is found.
     """
-    results_dir = create_output_dir(instances_path, "results")
+    try:
+        results_dir = create_output_dir(instances_path, "results")
+        load_api_keys(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config', 'api_keys.json'))
+        set_openai_api_configurations()
+        durations = []
 
-    load_api_keys(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config', 'api_keys.json'))
-    set_openai_api_configurations()
-    durations = []
-
-    for attempt in range(1, max_attempts + 1):
-        start_time = time.time()
-        
-        try:
-            prompt = generate_dl_prompts_from_json(instances_path, ontology_path)
-            response = str(send_prompt(prompt)).strip()
-
-            logging.info(f"Attempt {attempt} response: {response}")
-
-            instances = json.load(open(instances_path))
-            ontology = load_ontology(ontology_path)
-
-            verified_response = concept_verifier(instances, ontology, response)
+        for attempt in range(1, max_attempts + 1):
+            start_time = time.time()
             
-            logging.info(f"Attempt {attempt} verified response: {verified_response}")
+            try:
+                prompt = generate_dl_prompts_from_json(instances_path, ontology_path)
+                response = str(send_prompt(prompt)).strip()
 
-            if verified_response:
-                result, error = evaluator(instances, verified_response)
+                logging.info(f"Attempt {attempt} response: {response}")
 
-            else:
+                instances = json.load(open(instances_path))
+                ontology = load_ontology(ontology_path)
+
+                verified_response = concept_verifier(instances, ontology, response)
+                
+                logging.info(f"Attempt {attempt} verified response: {verified_response}")
+
+                if verified_response:
+                    result, error = evaluator(instances, verified_response)
+
+                else:
+                    result = False
+                    error = "Error verifying the candidate concept." 
+
+            except Exception as e:
+                logging.error(f"Error in attempt {attempt}: {e}")
                 result = False
-                error = "Error verifying the candidate concept." 
+                error = f"Error in attempt {attempt}: {e}"
 
-        except Exception as e:
-            logging.error(f"Error in attempt {attempt}: {e}")
-            result = False
-            error = f"Error in attempt {attempt}: {e}"
+            durations.append(time.time() - start_time)
 
-        durations.append(time.time() - start_time)
-
-        result_path = os.path.join(results_dir, f"result_{attempt}.txt")
-        with open(result_path, 'w') as f:
-            f.write(f"Prompt: {prompt}\nResponse: {response}\nVerified Response: {verified_response}\nError: {error}\nResult: {result}\n")
-            logging.info(f"Attempt {attempt} results saved to: {result_path}")
-        if result:
-            time.sleep(10)
-            return response, durations
-        time.sleep(10)
-
+            result_path = os.path.join(results_dir, f"result_{attempt}.txt")
+            with open(result_path, 'w') as f:
+                f.write(f"Prompt: {prompt}\nResponse: {response}\nVerified Response: {verified_response}\nError: {error}\nResult: {result}\n")
+                logging.info(f"Attempt {attempt} results saved to: {result_path}")
+            if result:
+                return response, durations
+            time.sleep(1)
+    except Exception as e:
+        logging.error(f"An error occurred during processing: {e}")
     return None, durations
 
 def main():
 
-    with open("dl_times.csv", 'w', newline='') as file:
+    with open("dl_times.csv", 'a+', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Separation Type', 'Trace', 'Duration', "Result"])
 
--- scripts/ltl_process.py
+++ scripts/ltl_process.py
@@ -15,9 +15,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def create_output_dir(trace_path, folder_name):
     parts = trace_path.split('/')
     constraint_type = parts[2]
-    file_id = os.path.splitext(parts[3])[0] 
-
-    output_dir = os.path.join("output", "LTL_process", constraint_type, file_id, folder_name)
+    
+    output_dir = os.path.join("output", "LTL_process", constraint_type, folder_name)
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -25,69 +24,63 @@ def create_output_dir(trace_path, folder_name):
 
 def ltl_process(trace_path, nuxmv_path='NUXMV', max_attempts=5):
     """
-    Generate LTL candidates until a correct LTL process formula is found.
+    Generate LTL candidates until a correct LTL formula is found.
 
     Parameters:
         trace_path (str): Path to the JSON trace file.
-        nuxmv_path (str): Path to the NuSMV executable.
-        max_attempts (int): Maximum number of attempts to find a valid LTL formula through the LLM.
+        nuxmv_path (str): Path to the NUXMV executable.
+        max_attempts (int): Maximum number of attempts to find a valid LTL formula.
 
     Returns:
-        str: A correct LTL process formula if found, None otherwise.
+        response(str): A correct LTL formula if found, None otherwise.
         int: The number of attempts made until a correct formula is found.
     """
-    try:
-        validate_json_structure(read_json_data(trace_path))
-        traces_smv_dir = create_output_dir(trace_path, "traces_smv")
-        generate_smv_files_from_json(trace_path, traces_smv_dir)
-        results_dir = create_output_dir(trace_path, "results")
-        load_api_keys(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config', 'api_keys.json'))
-        set_openai_api_configurations()
-        durations = []
+    validate_json_structure(read_json_data(trace_path))
+    traces_smv_dir = create_output_dir(trace_path, "traces_smv")
+    results_dir = create_output_dir(trace_path, "results")
+    generate_smv_files_from_json(trace_path, traces_smv_dir)
+    load_api_keys(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config', 'api_keys.json'))
+    set_openai_api_configurations()
+    durations = []
 
-        for attempt in range(1, max_attempts + 1):
-            start_time = time.time()
-            error = None
-            spec_path = None
-            prompt = generate_ltl_prompts_from_json(trace_path)
-            response = send_prompt(prompt).strip().replace('"', '')
-            logging.info(f"Attempt {attempt} response: {response}")
-            try:
-                f2i_response = f2i(response)
-                logging.info(f"Attempt {attempt} f2i: {f2i_response}")
-                if f2i_response:
-                    spec_dir = create_output_dir(trace_path, "spec")
-                    spec_path = generate_smv_spec(f2i_response, spec_dir)
-                    constraints = get_ltl_constraints(read_json_data(trace_path))
-                    if constraints and not all(char in constraints for char in response if char.islower()):
-                        error = "The generated LTL formula does not satisfy the signature restrictions."
-                    result, spec_error = evaluator(traces_smv_dir, spec_path)
-                    if spec_error:
-                        error = spec_error
-                else:
-                    result = False
-                    error = "Syntax error in the candidate formula."
-            except Exception as e:
-                logging.error(f"Error processing LTL formula: {e}")
+    for attempt in range(1, max_attempts + 1):
+        start_time = time.time()
+        error = None
+        spec_path = None
+        result = None
+        prompt = generate_ltl_prompts_from_json(trace_path)
+        response = send_prompt(prompt).strip().replace('"', '')
+        logging.info(f"Attempt {attempt} response: {response}")
+        try:
+            f2i_response = f2i(response)
+            logging.info(f"Attempt {attempt} f2i: {f2i_response}")
+            if f2i_response:
+                spec_dir = create_output_dir(trace_path, "spec")
+                spec_path = generate_smv_spec(f2i_response, spec_dir)
+                constraints = get_ltl_constraints(read_json_data(trace_path))
+                if constraints and not all(char in constraints for char in response if char.islower()):
+                    error = "The generated LTL formula does not satisfy the signature restrictions."
+                result, spec_error = evaluator(traces_smv_dir, spec_path)
+                if spec_error:
+                    error = spec_error
+            else:
                 result = False
-                response = None
-                f2i_response = None
-                error = str(e)
+                error = "Syntax error in the candidate formula."
+        except Exception as e:
+            logging.error(f"Error processing LTL formula: {e}")
+            error = str(e)
 
-            durations.append(time.time() - start_time)
-            result_path = os.path.join(results_dir, f"attempt_{attempt}.txt")
-            with open(result_path, 'w') as file:
-                file.write(f"Attempt: {attempt}\nPrompt: {prompt}\nResponse: {response}\nF2I Response: {f2i_response}\nError: {error}\nSpec Path: {spec_path if spec_path else 'None'}\nEvaluated Result: {result}\n")
-                logging.info(f"Attempt {attempt} results saved to: {result_path}")
-            if result:
-                return response, durations
-            time.sleep(10)
-    except Exception as e:
-        logging.error(f"An error occurred during processing: {e}")
+        durations.append(time.time() - start_time)
+        result_path = os.path.join(results_dir, f"attempt_{attempt}.txt")
+        with open(result_path, 'w') as file:
+            file.write(f"Attempt: {attempt}\nPrompt: {prompt}\nResponse: {response}\nF2I Response: {f2i_response if 'f2i_response' in locals() else 'None'}\nError: {error}\nSpec Path: {spec_path if spec_path else 'None'}\nEvaluated Result: {'True' if 'result' in locals() and result else 'False'}\n")
+            logging.info(f"Attempt {attempt} results saved to: {result_path}")
+        if result:
+            return response, durations
     return None, durations
 
 def main():
-
+    
     with open("ltl_times.csv", 'a+', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Constraint Type', 'Trace', 'Duration', "Result"])
